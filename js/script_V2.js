1af0bcad9fedad4b0bea56e5441e8bba679ba1e3
Eimayto <louis04060@gmail.com>
2018-11-19 17:05:35 +0900

    Update script_V2.js

--- js/script_V2.js
+++ js/script_V2.js
@@ -88,7 +88,7 @@ function On1Click(){
 function button1(choose){
   var r;
   var message = '';
-  var file = "http://www.10000recipe.com/index.html";
+  var file = "https://raw.githubusercontent.com/Eimayto/forcontest/master/download/mainpage.txt";
   var for_for;
   var rawFile = new XMLHttpRequest();
   rawFile.open("GET", file, false);
